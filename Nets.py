d73fded1f9eda83fc095ac68242d44236085e36b
hojat allah salehi <hsale014@diamond.cs.fiu.edu>
2024-08-12 22:33:43 -0400

    update the Net.py file and removed unnecessary models

--- Nets.py
+++ Nets.py
@@ -4,82 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class VGG16(nn.Module):
-    def __init__(self, num_classes=10):
-        super(VGG16, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 1 * 1, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-# Small2VGG Model
-class Small2VGG(nn.Module):
-    def __init__(self):
-        super(Small2VGG, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(64 * 8 * 8, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
 
 # Small2VGG Model for MNIST
 class CNNMNIST(nn.Module):
@@ -101,43 +25,6 @@ class CNNMNIST(nn.Module):
         x = self.fc2(x)
         return x
 
-# AlexNet Model
-class AlexNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 192, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((2, 2))
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(256 * 2 * 2, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        logits = self.classifier(x)
-        probas = F.softmax(logits, dim=1)
-        return logits
 
 # BasicBlock for ResNet18
 class BasicBlock(nn.Module):
@@ -228,52 +115,3 @@ class Bottleneck(nn.Module):
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
-def ResNet50():
-    return ResNet(Bottleneck, [3, 4, 6, 3])
-
-def ResNet101():
-    return ResNet(Bottleneck, [3, 4, 23, 3])
-
-class CNN2CIFAR(nn.Module):
-    def __init__(self, num_classes=10):
-        super(CNN2CIFAR, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)  # Adjusted for CIFAR-10
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)  # Adjusted for CIFAR-10
-        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout1 = nn.Dropout(0.25)
-        self.fc1 = nn.Linear(64 * 16 * 16, 128)  # Adjusted for CIFAR-10
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))  # Conv2d layer
-        x = F.relu(self.conv2(x))  # Conv2d layer
-        x = self.max_pool(x)  # MaxPool2d layer
-        x = self.dropout1(x)  # Dropout layer
-        x = torch.flatten(x, 1)  # Flatten layer
-        x = F.relu(self.fc1(x))  # Dense layer
-        x = self.dropout2(x)  # Dropout layer
-        x = self.fc2(x)  # Dense layer
-        return F.log_softmax(x, dim=1)
-
-class CNN2FMnist(nn.Module):
-    def __init__(self, num_classes=62):
-        super(CNN2FMnist, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
-        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout1 = nn.Dropout(0.25)
-        self.fc1 = nn.Linear(9216, 128)  # Flatten layer size should be calculated based on input dimensions
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))  # Conv2d layer
-        x = F.relu(self.conv2(x))  # Conv2d layer
-        x = self.max_pool(x)  # MaxPool2d layer
-        x = self.dropout1(x)  # Dropout layer
-        x = torch.flatten(x, 1)  # Flatten layer
-        x = F.relu(self.fc1(x))  # Dense layer
-        x = self.dropout2(x)  # Dropout layer
-        x = self.fc2(x)  # Dense layer
-        return F.log_softmax(x, dim=1)
